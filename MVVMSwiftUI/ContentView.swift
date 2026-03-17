12aaf636e2b50e6e167aa8216a3e415093e974f1
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2026-03-17 19:01:09 +0530

    Initial Commit

---
+++ MVVMSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMSwiftUI
+//
+//  Created by G Yacobu on 17/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by G Yacobu on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
